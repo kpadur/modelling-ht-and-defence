68d01c970e23d93faefaf2c8114094e23ca46596
kpadur <31754805+kpadur@users.noreply.github.com>
2025-04-08 11:16:38 +0300

    Update a2c_def_agent.py

--- a2c_def_agent.py
+++ a2c_def_agent.py
@@ -3,10 +3,8 @@ import numpy as np
 
 from nns import Defender1_nn, Defender2_nn
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # currently cpu
-
 class A2CServiceProvider:
-    """Service providers' behaviour"""
+    """Service provider's behaviour"""
 
     def __init__(self, state_space_defenders, n_filter, n_answer, dnn1_lr, dnn2_lr, device):
         super().__init__()
@@ -28,15 +26,9 @@ class A2CServiceProvider:
     def sample_actions(self, state):
         """
         Filters out the malicious traffic from the legitimate traffic
-        at this time step (Response)
-        defender:   defender ID
-        state:      numpy binary array of shape (n_agents),
-                    represemts whether each agent made a request (1) or not (0)
-        return:     numpy binary array of shape (n_agents),
-                    the final decision on each agent's request after applying
-                    the actions decided by the defender
+        and answers to agents in the social network at this time step.
         """
-        req_state = torch.tensor(state[0], device=device, dtype=torch.float32).unsqueeze(0)
+        req_state = torch.tensor(state[0], device=self.device, dtype=torch.float32).unsqueeze(0)
 
         with torch.no_grad():
             # Get the probabilities of how likely the defender should block the request for each agent
@@ -50,7 +42,7 @@ class A2CServiceProvider:
         # Apply 0 to agents who did not request service
         actions = actions * state[0]
 
-        sn_state = torch.tensor(state[1], device=device, dtype=torch.float32).unsqueeze(0) # should be state 1 and 2 
+        sn_state = torch.tensor(state[1], device=self.device, dtype=torch.float32).unsqueeze(0)
         with torch.no_grad():
             # Get the probabilities of how likely the defender should reply to each agent
             logits_answer, _ = self.answer_nn(sn_state)
@@ -66,7 +58,9 @@ class A2CServiceProvider:
         return actions, sn_actions
 
     def compute_a2c_loss(self, states, all_actions, total_rewards, gamma_dnn1, gamma_dnn2, entropy_coef):
-
+        """
+        Compute the losses for filtering and replying decisions.
+        """
         state_requests = np.array([s[0] for s in states])
         state_sn = np.array([s[1] for s in states])
         filters = np.array([t[0] for t in all_actions])
@@ -74,42 +68,21 @@ class A2CServiceProvider:
         rewards = np.array([r[0] for r in total_rewards])
         feedback = np.array([r[1] for r in total_rewards])
 
-        n_timesteps, n_actions = rewards.shape
-        rewards_to_go = np.zeros_like(rewards)
-        rewards_to_go[-1] = rewards[-1]
-
-        for t in range(n_timesteps - 2, -1, -1):
-            for a in range(n_actions):
-                rewards_to_go[t, a] = gamma_dnn1 * rewards_to_go[t + 1, a] + rewards[t, a]
-        
-
-        n_timesteps, n_actions = rewards.shape
-        feedback_to_go = np.zeros_like(feedback)
-        feedback_to_go[-1] = feedback[-1]
-
-        for t in range(n_timesteps - 2, -1, -1):
-            for a in range(n_actions):
-                feedback_to_go[t, a] = gamma_dnn2 * feedback_to_go[t + 1, a] + feedback[t, a]
+        # Calculate rewards to go
+        rewards_to_go = self.compute_rewards_to_go(rewards, gamma_dnn1)
+        feedback_to_go = self.compute_rewards_to_go(feedback, gamma_dnn2)
 
         # Convert to tensors
-        state_requests = torch.tensor(state_requests, device=device, dtype=torch.float)
-        state_sn = torch.tensor(state_sn, device=device, dtype=torch.float)
-        filters = torch.tensor(filters, device=device, dtype=torch.long)
-        answers = torch.tensor(answers, device=device, dtype=torch.long)
-        rewards_to_go = torch.tensor(rewards_to_go, device=device, dtype=torch.float)
-        feedback_to_go = torch.tensor(feedback_to_go, device=device, dtype=torch.float)
+        state_requests = torch.tensor(state_requests, device=self.device, dtype=torch.float)
+        state_sn = torch.tensor(state_sn, device=self.device, dtype=torch.float)
+        filters = torch.tensor(filters, device=self.device, dtype=torch.long)
+        answers = torch.tensor(answers, device=self.device, dtype=torch.long)
+        rewards_to_go = torch.tensor(rewards_to_go, device=self.device, dtype=torch.float)
+        feedback_to_go = torch.tensor(feedback_to_go, device=self.device, dtype=torch.float)
 
+        # Calculate loss
         logits_f, state_values_f = self.filter_nn(state_requests)
-        probs_f = torch.sigmoid(logits_f) # using sigmoid function
-        log_probs_f = torch.log(probs_f) # shape ([timestep, agents]) # has to be changed
-        log_probs_for_filters = log_probs_f*filters + (1-log_probs_f)*(1-filters) # used for classification
-
-        # Calculate advantages for each agent and then average them
-        advantage_f = rewards_to_go - state_values_f.unsqueeze(1)
-
-        J_f = (log_probs_for_filters*advantage_f.detach()).mean(1).mean()
-        H_f = -(probs_f*log_probs_for_filters).sum(-1).mean()
-        loss_f = -(J_f + entropy_coef*H_f)
+        loss_f = self.compute_policy_loss(logits_f, state_values_f, rewards_to_go, filters, entropy_coef)
 
         # Backpropagate loss
         self.filter_opt.zero_grad()
@@ -117,17 +90,9 @@ class A2CServiceProvider:
         torch.nn.utils.clip_grad_norm_(self.filter_nn.parameters(), max_norm=0.5)
         self.filter_opt.step()
 
+        # Calculate loss
         logits_a, state_values_a = self.answer_nn(state_sn)
-        probs_a = torch.sigmoid(logits_a) # using sigmoid function
-        log_probs_a = torch.log(probs_a) # shape ([timestep, agents]) # has to be changed
-        log_probs_for_answers = log_probs_a*filters + (1-log_probs_a)*(1-answers) # used for classification
-
-        # Calculate advantages for each agent and then average them
-        advantage_a = feedback_to_go - state_values_a.unsqueeze(1)
-
-        J_a = (log_probs_for_answers*advantage_a.detach()).mean(1).mean()
-        H_a = -(probs_a*log_probs_for_answers).sum(-1).mean()
-        loss_a = -(J_a + entropy_coef*H_a)
+        loss_a = self.compute_policy_loss(logits_a, state_values_a, feedback_to_go, answers, entropy_coef)
 
         # Backpropagate loss
         self.answer_opt.zero_grad()
@@ -136,3 +101,25 @@ class A2CServiceProvider:
         self.answer_opt.step()
 
         return loss_f, loss_a
+    
+    @staticmethod
+    def compute_rewards_to_go(rewards, gamma):
+        n_timesteps, n_actions = rewards.shape
+        rewards_to_go = np.zeros_like(rewards)
+        rewards_to_go[-1] = rewards[-1]
+
+        for t in range(n_timesteps - 2, -1, -1):
+            for a in range(n_actions):
+                rewards_to_go[t, a] = gamma * rewards_to_go[t + 1, a] + rewards[t, a]
+        return rewards_to_go
+
+    @staticmethod
+    def compute_policy_loss(logits, state_values, rewards_to_go, actions, entropy_coef):
+        probs = torch.sigmoid(logits) # using sigmoid function
+        log_probs = torch.log(probs)
+        log_probs_for_actions = log_probs*actions + (1-log_probs)*(1-actions)
+        # Calculate advantages for each agent and then average them
+        advantage = rewards_to_go - state_values.unsqueeze(1)
+        J = (log_probs_for_actions*advantage.detach()).mean(1).mean()
+        H = -(probs*log_probs_for_actions).sum(-1).mean()
+        return -(J + entropy_coef * H)
